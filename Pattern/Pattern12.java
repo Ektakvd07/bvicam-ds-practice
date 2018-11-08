3f78dfde1aae4dc0d319a542a2154d7dd12a9960
Ektakvd07 <44539548+Ektakvd07@users.noreply.github.com>
2018-11-08 12:12:09 +0530

    Update Pattern12.java

--- Pattern/Pattern12.java
+++ Pattern/Pattern12.java
@@ -1,14 +1,51 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ WAP to print pattern
+
+123456789
+12345678
+123456
+12345
+123
+12
+98
+987
+98765
+987654
+98765432
+987654321
+
  */
-package pattern;
+package pattern.Pattern;
 
 public class Pattern12 {
     public static void main(String args[])
     {
         int i,j;
+        for(i=6;i>=0;i=i-3)
+        {
+            for(j=1;j<=i+3;j++)
+            {System.out.print(j);
+                
+            }System.out.println();
+            for(j=1;j<=i+2;j++)
+            {
+               System.out.print(j);
+            }
+            System.out.println();
+        }
+        for(i=6;i>=0;i=i-3)
+        {
+            for(j=9;j>=i+2;j--)
+            {
+                System.out.print(j);
+            }
+            System.out.println();
+            for(j=9;j>=i+1;j--)
+            {   System.out.print(j);
+                
+            }
+            System.out.println();
+        }
         
     }
     
